4aab4301e5a889c85d07aa092cf5e9638a69addd
Rahul Rajput <newasram86@gmail.com>
2025-10-22 20:18:16 +0530

    feat: shadcn comp

---
+++ components/ui/theme-changer.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ThemeChanger() {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  return (
+    <Button
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      size="icon-sm"
+      variant="ghost"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="size-4"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+        <path d="M12 3l0 18"></path>
+        <path d="M12 9l4.65 -4.65"></path>
+        <path d="M12 14.3l7.37 -7.37"></path>
+        <path d="M12 19.6l8.85 -8.85"></path>
+      </svg>
+    </Button>
+  );
+}
