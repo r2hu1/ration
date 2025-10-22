350ee237647d3ae4aed147516d95c2638fa6d55a
Rahul Rajput <newasram86@gmail.com>
2025-10-22 20:50:51 +0530

    feat: auth context for easy data accessing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { AuthProvider } from "@/components/providers/auth-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <AuthProvider>{children}</AuthProvider>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/providers/auth-context.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useSession } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect } from "react";
+
+const AuthContext = createContext<any>(null);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { data, error, isPending, refetch } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isPending && !data?.session) {
+      router.push("/auth/login-or-create-account");
+    }
+  }, [isPending]);
+
+  return (
+    <AuthContext.Provider value={{ data, error, isPending, refetch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthState = () => useContext(AuthContext);
