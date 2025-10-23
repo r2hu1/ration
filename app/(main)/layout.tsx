e9c44901c9e5b5c55e3baae0ed320186794ef778
Rahul Rajput <newasram86@gmail.com>
2025-10-23 19:46:59 +0530

    added trpc react provider

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,14 @@
 import { AuthProvider } from "@/components/providers/auth-context";
 import Header from "@/modules/dashboard/views/ui/header";
+import { TRPCReactProvider } from "@/trpc/client";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
+    <TRPCReactProvider>
       <AuthProvider>
         <Header />
         <div className="px-6 md:px-20 py-10">{children}</div>
       </AuthProvider>
-    </main>
+    </TRPCReactProvider>
   );
 }
