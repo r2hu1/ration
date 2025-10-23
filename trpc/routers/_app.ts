a52fc62fd197821f4ef291b01bacdceef89e5f0e
Rahul Rajput <newasram86@gmail.com>
2025-10-23 14:22:29 +0530

    feat: setuped trpc client and server

---
+++ trpc/routers/_app.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
+export const appRouter = createTRPCRouter({});
+// export type definition of API
+export type AppRouter = typeof appRouter;
