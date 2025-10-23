d0f2d7779854be93480c1fa6f9a25e17716b9116
Rahul Rajput <newasram86@gmail.com>
2025-10-23 13:17:45 +0530

    feat: progressive create team

--- modules/dashboard/views/ui/create-team.tsx
+++ modules/dashboard/views/ui/create-team.tsx
@@ -10,34 +10,56 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function CreateTeam({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isMobile = useIsMobile();
+  const Slot = isMobile ? Drawer : AlertDialog;
+  const SlotContent = isMobile ? DrawerContent : AlertDialogContent;
+  const SlotTrigger = isMobile ? DrawerTrigger : AlertDialogTrigger;
+  const SlotTitle = isMobile ? DrawerTitle : AlertDialogTitle;
+  const SlotDescription = isMobile ? DrawerDescription : AlertDialogDescription;
+  const SlotHeader = isMobile ? DrawerHeader : AlertDialogHeader;
+  const SlotFooter = isMobile ? DrawerFooter : AlertDialogFooter;
+  const SlotAction = isMobile ? Button : AlertDialogAction;
+  const SlotCancel = isMobile ? DrawerClose : AlertDialogCancel;
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Create Team</AlertDialogTitle>
-          <AlertDialogDescription>
+    <Slot>
+      <SlotTrigger asChild>{children}</SlotTrigger>
+      <SlotContent>
+        <SlotHeader>
+          <SlotTitle>Create Team</SlotTitle>
+          <SlotDescription>
             A team can have multiple members and can be used to organize and
             share projects.
-          </AlertDialogDescription>
+          </SlotDescription>
           <form className="py-2 grid gap-2">
             <Label>Team Name</Label>
             <Input className="w-full" placeholder="Unique Team" />
           </form>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+        </SlotHeader>
+        <SlotFooter className="sm:flex grid grid-cols-2 gap-3">
+          <SlotCancel>Cancel</SlotCancel>
+          <SlotAction>Continue</SlotAction>
+        </SlotFooter>
+      </SlotContent>
+    </Slot>
   );
 }
