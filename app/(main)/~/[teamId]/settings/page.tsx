c6858e9706462d994119bab423c1fea1b57a903e
Rahul Rajput <newasram86@gmail.com>
2025-10-24 23:40:48 +0530

    feat: 1st layer of settings

--- app/(main)/~/[teamId]/settings/page.tsx
+++ app/(main)/~/[teamId]/settings/page.tsx
@@ -1,3 +1,14 @@
-export default async function Settings() {
-  return <div>Settings</div>;
+import TeamSettings from "@/modules/dashboard/views/ui/settings";
+
+export default async function Settings({
+  params,
+}: {
+  params: { teamId: string };
+}) {
+  const param = await params;
+  return (
+    <div>
+      <TeamSettings slug={param.teamId} />
+    </div>
+  );
 }
---
+++ modules/dashboard/views/ui/settings/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useAuthState } from "@/components/providers/auth-context";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import { LogOut, Trash2 } from "lucide-react";
+import Link from "next/link";
+
+export default function TeamSettings({ slug }: { slug: string }) {
+  const trpc = useTRPC();
+  const { data, isPending } = useQuery(
+    trpc.teams.get_by_slug.queryOptions({ slug }),
+  );
+  const { data: user } = useAuthState();
+  return (
+    <div>
+      {/*<div className="mt-6">
+        <h1 className="text-sm font-medium">Settings</h1>
+      </div>*/}
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-lg font-medium">{data?.name}</h1>
+          <p className="text-sm text-foreground/80">
+            {data?.owner === user?.session?.userId
+              ? "You created this team"
+              : "Someone invited you to this team"}
+          </p>
+        </div>
+        {data?.owner === user?.session?.userId ? (
+          <Button size="sm" asChild>
+            <Link href={`/~/${slug}/danger`}>
+              Delete <Trash2 className="size-3.5" />
+            </Link>
+          </Button>
+        ) : (
+          <Button size="sm">
+            Leave <LogOut className="size-3.5" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
