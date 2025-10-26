acda809a3593af81c00c624878fa6176f6a4fcdd
Rahul Rajput <newasram86@gmail.com>
2025-10-27 00:31:52 +0530

    feat: fix suspense

--- modules/dashboard/views/ui/personal/all-projects.tsx
+++ modules/dashboard/views/ui/personal/all-projects.tsx
@@ -8,7 +8,7 @@ import ProjectCard from "./project-card";
 import { Loader } from "@/components/ui/loader";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function AllPersonalProjects() {
@@ -40,10 +40,20 @@ export default function AllPersonalProjects() {
   if (!isPending && !projects) return <EmptyProject />;
 
   return (
-    <div className={cn("grid", viewType == "flex" ? flexView : gridView)}>
-      {projects.map((project) => (
-        <ProjectCard viewType={viewType} project={project} key={project.id} />
-      ))}
-    </div>
+    <Suspense
+      fallback={
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {Array.from({ length: 5 }, (_, i) => (
+            <Skeleton className="h-16 w-full" key={i} />
+          ))}
+        </div>
+      }
+    >
+      <div className={cn("grid", viewType == "flex" ? flexView : gridView)}>
+        {projects.map((project) => (
+          <ProjectCard viewType={viewType} project={project} key={project.id} />
+        ))}
+      </div>
+    </Suspense>
   );
 }
