9b187bf406f9e5c30ffc4e214c4999f0a337e4b5
Rahul Rajput <newasram86@gmail.com>
2025-10-27 23:40:29 +0530

    feat: empty state

--- modules/dashboard/views/ui/personal/personal-project-view.tsx
+++ modules/dashboard/views/ui/personal/personal-project-view.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
-import { ArrowLeft, Bolt, Copy, Download } from "lucide-react";
+import { AlertTriangle, ArrowLeft, Bolt, Copy, Download } from "lucide-react";
 import PersonalProjectSettings from "./project-settings";
 import ChangeProjectType, { ProjectType } from "./change-type";
 import Link from "next/link";
@@ -90,6 +90,19 @@ export default function PersonalProjectView({
       </div>
 
       <div className="grid gap-3">
+        {!isPending && !project.envs && (
+          <div className="flex mt-5 items-center justify-center border h-[300px] w-full">
+            <div className="text-center grid gap-2">
+              <AlertTriangle className="size-4 mx-auto" />
+              <h1 className="text-sm -mb-1 mt-px text-foreground/90">
+                Its empty here
+              </h1>
+              <p className="text-xs text-foreground/70">
+                Start by creating an environment.
+              </p>
+            </div>
+          </div>
+        )}
         {Object.entries(project.envs as Record<string, any>).map(
           ([key, value]) => (
             <EnvCard key={key} kkey={key} value={value} />
