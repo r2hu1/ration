e58ef30a279bdc3b15c1734b149f14c1750460d2
r2hu1 <newasram86@gmail.com>
2026-04-28 20:49:50 +0530

    up

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -17,13 +17,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
-      }}
       style={
         {
           "--normal-bg": "var(--popover)",
